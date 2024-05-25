a469bf48baeb501e17770a4da191d91c4a52bbe2
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-05-25 12:22:20 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import CategoriesPage from "./pages/CategoriesPage/CategoriesPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import AccountPage from "./pages/Account/AccountPage";
 import ProductPageInCategory from "./pages/ProductPageInCategory/ProductPageInCategory";
-import {useAppDispatch} from "./hooks/redux";
+import {useAppDispatch, useAppSelector} from "./hooks/redux";
 import {checkTocken} from "./utils/checkAuth";
 import {deleteUser} from "./store/reducers/userSlice";
 import ProductPage from "./pages/ProductPage/ProductPage";
+import ProtectedRoute from "./pages/ProtectedRoute/ProtectedRoute";
 
 
 function App() {
+    // const user = useAppSelector(state => state.user)
     const dispatch = useAppDispatch();
     !checkTocken()&&dispatch(deleteUser())
     return (
@@ -21,9 +23,11 @@ function App() {
                 <Route path={'/categories'} element={<CategoriesPage/>}/>
                 <Route path={'/categories/:id'} element={<ProductPageInCategory/>}/>
                 <Route path={'/register'} element={<RegisterPage/>}/>
-                <Route path={'/account'} element={<AccountPage/>}/>
                 <Route path={'/product/:id'} element={<ProductPage/>}/>
                 <Route path={'/favorite'} element={<></>}/>
+                {/*<Route element={<ProtectedRoute isAllow={!!user}/>}>*/}
+                {/*    <Route path={'/account'} element={<AccountPage/>}/>*/}
+                {/*</Route>*/}
             </Routes>
         </div>
     );
