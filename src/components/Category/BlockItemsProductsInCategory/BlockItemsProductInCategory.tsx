12dc6695ff6663e065ab951cec4614f43896c842
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-05-26 22:52:39 +0300

    add_route_and_page_find_products

--- src/components/Category/BlockItemsProductsInCategory/BlockItemsProductInCategory.tsx
+++ src/components/Category/BlockItemsProductsInCategory/BlockItemsProductInCategory.tsx
@@ -1,15 +1,16 @@
 import {FC, useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
-import {initialProductDTOArray, ProductDTO} from "../../../api/dto/product.dto";
 import * as Api from "../../../api";
 import ItemProductInPageCategory from "../ItemProductInPageCategory/ItemProductInPageCategory";
 import './BlockItemsProductsInCategory.scss'
+import {ProductDTO} from "../../../api/dto/product.dto";
 
 const BlockItemsProductInCategory: FC = () => {
-    const {id} = useParams()
-    const [products, setProducts] = useState<ProductDTO[]>(initialProductDTOArray)
+    const {id,name} = useParams()
+    const [products, setProducts] = useState<ProductDTO[]>([])
     useEffect(() => {
         if (id) Api.products.getAllProductsByIDCategory(id).then(setProducts)
+        if (name) Api.products.findAllProductByPartName(name).then(setProducts)
     }, []);
     return (
         <div className={'block_items_products_in_category'}>
--- src/components/SearchCatalog/SearchCatalog.tsx
+++ src/components/SearchCatalog/SearchCatalog.tsx
@@ -3,23 +3,35 @@ import icon_search from "../../images/icons_search.svg";
 import './SearchCatalog.scss'
 import useDebounce from "../../hooks/useDebounce";
 import * as Api from '../../api'
-import {initialProductDTOArray, ProductDTO} from "../../api/dto/product.dto";
 import ArrayFindItemsProducts from "../ArrayFindItemsProducts/ArrayFindItemsProducts";
+import {ProductDTO} from "../../api/dto/product.dto";
+import {useNavigate} from "react-router-dom";
 
-const SearchCatalog:FC = ()=>{
-    const [valueInput,setValueInput] = useState<string>('');
-    const [arrayProducts,setArrayProducts] = useState<ProductDTO[]>(initialProductDTOArray)
+const SearchCatalog: FC = () => {
+    const [valueInput, setValueInput] = useState<string>('');
+    const [arrayProducts, setArrayProducts] = useState<ProductDTO[]>([])
     const valueInputDebounce = useDebounce<string>(valueInput)
+
+    const navigate = useNavigate()
+
+    const onKeyDownInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key === 'Enter') navigate(`/search/${valueInputDebounce}`)
+    }
+
     useEffect(() => {
-        if(valueInputDebounce!=='') Api.products.findProductByPartName(valueInputDebounce).then(setArrayProducts)
-        else setArrayProducts(initialProductDTOArray)
+        if (valueInputDebounce !== '') Api.products.findTenProductByPartName(valueInputDebounce).then(setArrayProducts)
+        else setArrayProducts([])
     }, [valueInputDebounce]);
+
     return (
-        <div style={{display:'flex',flexDirection:'column'}}>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
             <div className={'wrapperInputInHeader'}>
-                <input className={'inputInHeader'} type={'text'} placeholder={'Поиск по каталогу'}
-                       onChange={(e)=>setValueInput(e.target.value)}/>
-                <img className={'imgInInput'} src={icon_search} alt={'Поиск'}/>
+                <input id={'inputInHeader'} type={'text'} placeholder={'Поиск по каталогу'}
+                       onChange={(e) => setValueInput(e.target.value)}
+                       onKeyDown={event => onKeyDownInput(event)}/>
+                <img id={'imgInInput'} src={icon_search} alt={'Поиск'} onClick={() => {
+                    navigate(`/search/${valueInputDebounce}`)
+                }}/>
             </div>
             <ArrayFindItemsProducts arrayProducts={arrayProducts}/>
         </div>
