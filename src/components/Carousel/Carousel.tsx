2adf823d0908bcc3a513ff000d192d4fcc7cfedb
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-06-13 00:56:07 +0300

    fix

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,25 +1,27 @@
 import 'react-alice-carousel/lib/alice-carousel.css';
-import sl1 from './images/sl1.jpg'
-import sl2 from './images/sl2.jpeg'
-import AliceCarousel from "react-alice-carousel";
-import './Carousel.scss'
-import {ControlsStrategy} from "react-alice-carousel/lib/types";
+import sl1 from './images/sl1.jpg';
+import sl11 from './images/sl1.png';
+import sl2 from './images/sl2.jpeg';
+import AliceCarousel from 'react-alice-carousel';
+import './Carousel.scss';
+import {ControlsStrategy} from 'react-alice-carousel/lib/types';
 import React from 'react';
 
 const Carousel = () => {
-
-	const items = [
-		<img src={sl1}/>,
-		<img src={sl2}/>
-	]
+	const items = [<img src={sl1} />, <img src={sl11} />, <img src={sl2} />];
 
 	return (
 		<div className={'wrapper_carousel'}>
-			<AliceCarousel mouseTracking autoPlay autoPlayInterval={3000} infinite items={items}
-			               controlsStrategy={ControlsStrategy.RESPONSIVE} disableButtonsControls/>
+			<AliceCarousel
+				mouseTracking
+				autoPlay
+				autoPlayInterval={3000}
+				infinite
+				items={items}
+				controlsStrategy={ControlsStrategy.RESPONSIVE}
+				disableButtonsControls
+			/>
 		</div>
-	)
-
-
-}
-export default Carousel
+	);
+};
+export default Carousel;
--- src/components/ModalLogIn/ModalLogIn.tsx
+++ src/components/ModalLogIn/ModalLogIn.tsx
@@ -1,104 +1,99 @@
-import React, {FC} from "react";
-import {Button, Checkbox, Form, Input, Modal, notification} from "antd";
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import {Link} from "react-router-dom";
-import {LoginFormDto} from "../../api/dto/auth.dto";
-import * as Api from '../../api'
-import {Cookies} from "react-cookie";
-import {useAppDispatch, useAppSelector} from "../../hooks/redux";
-import {addUser} from "../../store/reducers/userSlice";
+import React, {FC} from 'react';
+import {Button, Checkbox, Form, Input, Modal, notification} from 'antd';
+import {LockOutlined, UserOutlined} from '@ant-design/icons';
+import {Link} from 'react-router-dom';
+import {LoginFormDto} from '../../api/dto/auth.dto';
+import * as Api from '../../api';
+import {Cookies} from 'react-cookie';
+import {useAppDispatch} from '../../hooks/redux';
+import {addUser} from '../../store/reducers/userSlice';
 
 type ModalLogInProps = {
-    openLogin:boolean,
-    setOpenLogin:  React.Dispatch<React.SetStateAction<boolean>>
-}
+	openLogin: boolean;
+	setOpenLogin: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 const cookie = new Cookies();
 
+const ModalLogIn: FC<ModalLogInProps> = ({openLogin, setOpenLogin}) => {
+	// const userRedux = useAppSelector(state=>state.user)
+	const dispatch = useAppDispatch();
 
-const ModalLogIn: FC<ModalLogInProps> = ({openLogin,setOpenLogin}) => {
+	const onSubmit = async (values: LoginFormDto) => {
+		try {
+			const {token, user} = await Api.auth.login(values);
 
-    // const userRedux = useAppSelector(state=>state.user)
-    const dispatch = useAppDispatch();
+			dispatch(addUser(user));
 
-    const onSubmit = async (values:LoginFormDto)=>{
-        try{
-            const { token,user } = await Api.auth.login(values)
+			notification.success({
+				message: 'Успешно',
+				duration: 2,
+			});
 
-            dispatch(addUser(user))
+			cookie.set('_token', token, {path: '/'});
 
-            notification.success({
-                message:'Успешно',
-                duration:2
-            })
+			// window.location.reload()
+		} catch (err) {
+			notification.error({
+				message: 'Неверная почта или пароль',
+				duration: 2,
+			});
+		}
+	};
+	const handleCancel = () => {
+		setOpenLogin(false);
+	};
 
-            cookie.set('_token',token,{path:'/'})
+	return (
+		<>
+			<Modal
+				title={'Вход'}
+				open={openLogin}
+				onCancel={handleCancel}
+				onOk={() => handleCancel}
+				centered
+				footer={[]}
+			>
+				<Form
+					name="normal_login"
+					className="login-form"
+					initialValues={{remember: true}}
+					onFinish={(values: LoginFormDto) => {
+						onSubmit(values);
+					}}
+				>
+					<Form.Item name="email" rules={[{required: true, message: 'Пожалуйста,введите email'}]}>
+						<Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
+					</Form.Item>
+					<Form.Item name="password" rules={[{required: true, message: 'Пожалуйста, введите пароль'}]}>
+						<Input.Password
+							prefix={<LockOutlined className="site-form-item-icon" />}
+							type="password"
+							placeholder="Password"
+						/>
+					</Form.Item>
+					<Form.Item>
+						<Form.Item name="remember" valuePropName="checked" noStyle>
+							<Checkbox>Remember me</Checkbox>
+						</Form.Item>
 
-            // window.location.reload()
-        }
-        catch (err){
-            notification.error({
-                message:'Неверная почта или пароль',
-                duration:2
-            })
-        }
-    }
-    const handleCancel = () => {
-        setOpenLogin(false);
-    };
+						<a className="login-form-forgot" href="">
+							Forgot password
+						</a>
+					</Form.Item>
 
-    return (
-        <>
-            <Modal
-                title={'Вход'}
-                open={openLogin}
-                onCancel={handleCancel}
-                onOk={()=>handleCancel}
-                centered
-                footer={[]}
-            >
-                <Form
-                    name="normal_login"
-                    className="login-form"
-                    initialValues={{ remember: true }}
-                    onFinish={(values:LoginFormDto)=>{onSubmit(values)}}
-                >
-                    <Form.Item
-                        name="email"
-                        rules={[{ required: true, message: 'Пожалуйста,введите email' }]}
-                    >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" />
-                    </Form.Item>
-                    <Form.Item
-                        name="password"
-                        rules={[{ required: true, message: 'Пожалуйста, введите пароль' }]}
-                    >
-                        <Input.Password
-                            prefix={<LockOutlined className="site-form-item-icon" />}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Item>
-                    <Form.Item>
-                        <Form.Item name="remember" valuePropName="checked" noStyle>
-                            <Checkbox>Remember me</Checkbox>
-                        </Form.Item>
-
-                        <a className="login-form-forgot" href="">
-                            Forgot password
-                        </a>
-                    </Form.Item>
-
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" className="login-form-button">
-                            Log in
-                        </Button>
-                        Or <Link to={'/register'}>register now!</Link>
-                    </Form.Item>
-                </Form>
-
-            </Modal>
-        </>
-    )
-}
-export default ModalLogIn
+					<Form.Item>
+						<Button type="primary" htmlType="submit" className="login-form-button">
+							Log in
+						</Button>
+						Or{' '}
+						<Link to={'/register'} onClick={() => setOpenLogin(false)}>
+							register now!
+						</Link>
+					</Form.Item>
+				</Form>
+			</Modal>
+		</>
+	);
+};
+export default ModalLogIn;
