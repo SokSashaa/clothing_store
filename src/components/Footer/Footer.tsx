09afa0887ae8b263d3cb05375d16adcc5fa17e31
SokSashaa <127789980+SokSashaa@users.noreply.github.com>
2024-06-13 10:02:19 +0300

    fix3

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,90 +1,88 @@
-import {FC} from "react";
-import './Footer.scss'
+import {FC} from 'react';
+import './Footer.scss';
 import {routesMap} from '../../utils/routesMap';
 import {NavLink} from 'react-router-dom';
-import React from "react";
+import React from 'react';
 
 type itemsFooter = {
-    name: React.ReactNode,
-    link: string,
-    block: FooterBlocks,
-}
+	name: React.ReactNode;
+	link: string;
+	block: FooterBlocks;
+};
 
 enum FooterBlocks {
-    Company,
-    ToClient,
-    Contacts
+	Company,
+	ToClient,
+	Contacts,
 }
 
 const itemsNameInFooter: itemsFooter[] = [
-    {block: FooterBlocks.Company, name: 'О нас', link: routesMap.aboutPage},
-    {block: FooterBlocks.Company, name: 'Вакансии', link: routesMap.vacancies},
-    {block: FooterBlocks.Company, name: 'Новости', link: routesMap.news},
-    {block: FooterBlocks.Company, name: 'Категории', link: routesMap.categories},
-    {block: FooterBlocks.Company, name: 'Отзывы', link: routesMap.reviews},
-    
-    {block: FooterBlocks.ToClient, name: 'Акции', link: routesMap.discounts},
-    {block: FooterBlocks.ToClient, name: 'Доставка и оплата', link: routesMap.paymentAndDelivery},
-    {block: FooterBlocks.ToClient, name: 'Возвраты', link: routesMap.returns},
-    
-    {
-        block: FooterBlocks.Contacts,
-        name: <p><span>8(800)123-45-67</span> оптовые продажи</p>,
-        link: 'tel:8-800-123-45-67'
-    },
-    {
-        block: FooterBlocks.Contacts,
-        name: <p><span>8(800)123-45-68</span> розничные продажи</p>,
-        link: 'tel:8-800-123-45-68'
-    },
-    {
-        block: FooterBlocks.Contacts,
-        name: <p><span>mail@mail.ru</span></p>,
-        link: 'mailto:mail@mail.ru'
-    }
-]
+	{block: FooterBlocks.Company, name: 'О нас', link: routesMap.aboutPage},
+	{block: FooterBlocks.Company, name: 'Вакансии', link: routesMap.vacancies},
+	{block: FooterBlocks.Company, name: 'Новости', link: routesMap.news},
+	{block: FooterBlocks.Company, name: 'Категории', link: routesMap.categories},
+	{block: FooterBlocks.Company, name: 'Отзывы', link: routesMap.reviews},
 
+	{block: FooterBlocks.ToClient, name: 'Акции', link: routesMap.discounts},
+	{block: FooterBlocks.ToClient, name: 'Доставка и оплата', link: routesMap.paymentAndDelivery},
+	{block: FooterBlocks.ToClient, name: 'Возвраты', link: routesMap.returns},
+
+	{
+		block: FooterBlocks.Contacts,
+		name: (
+			<p>
+				<span>8(800)123-45-67</span> оптовые продажи
+			</p>
+		),
+		link: 'tel:8-800-123-45-67',
+	},
+	{
+		block: FooterBlocks.Contacts,
+		name: (
+			<p>
+				<span>8(800)123-45-68</span> розничные продажи
+			</p>
+		),
+		link: 'tel:8-800-123-45-68',
+	},
+	{
+		block: FooterBlocks.Contacts,
+		name: (
+			<p>
+				<span>mail@mail.ru</span>
+			</p>
+		),
+		link: 'mailto:mail@mail.ru',
+	},
+];
 
 const Footer: FC = () => {
-    return (
-        <footer className={'wrapper_footer'}>
-            <div className={'columnInFooter'}>
-                <h3>Компания</h3>
-                {
-                    itemsNameInFooter.map((item, index) => (
-                            item.block === FooterBlocks.Company && (
-                                <NavLink
-                                    key={index}
-                                    to={item.link}
-                                >
-                                    {item.name}
-                                </NavLink>
-                            )
-                        )
-                    )
-                }
-            </div>
-            <div className={'columnInFooter'}>
-                <h3>Клиенту</h3>
-                {
-                    itemsNameInFooter.map((item, index) => (
-                            item.block === FooterBlocks.ToClient && (
-                                <NavLink
-                                    key={index}
-                                    to={item.link}
-                                >
-                                    {item.name}
-                                </NavLink>
-                            )
-                        )
-                    )
-                }
-            </div>
-            <div className={'columnInFooter'}>
-                <h3>Контакты</h3>
-                {itemsNameInFooter.map((item, index) => item.block === FooterBlocks.Contacts && <a href={item.link} key={index}>{item.name}</a>)}
-            </div>
-        </footer>
-    )
-}
-export default Footer
+	return (
+		<footer className={'wrapper_footer'}>
+			<div className={'columnInFooter'}>
+				<h3>Компания</h3>
+				{itemsNameInFooter.map(
+					(item, index) => item.block === FooterBlocks.Company && <a key={index}>{item.name}</a>
+				)}
+			</div>
+			<div className={'columnInFooter'}>
+				<h3>Клиенту</h3>
+				{itemsNameInFooter.map(
+					(item, index) => item.block === FooterBlocks.ToClient && <a key={index}>{item.name}</a>
+				)}
+			</div>
+			<div className={'columnInFooter'}>
+				<h3>Контакты</h3>
+				{itemsNameInFooter.map(
+					(item, index) =>
+						item.block === FooterBlocks.Contacts && (
+							<a href={item.link} key={index}>
+								{item.name}
+							</a>
+						)
+				)}
+			</div>
+		</footer>
+	);
+};
+export default Footer;
--- src/components/Header/MiniHeader/MiniHeader.tsx
+++ src/components/Header/MiniHeader/MiniHeader.tsx
@@ -1,41 +1,32 @@
-import {FC} from "react";
-import './MiniHeader.scss'
-import {NavLink} from "react-router-dom";
+import {FC} from 'react';
+import './MiniHeader.scss';
+import {NavLink} from 'react-router-dom';
 import {routesMap} from '../../../utils/routesMap';
-import React from "react";
+import React from 'react';
 
 type itemsHeader = {
-	name: string,
-	link: string
-}
+	name: string;
+	link: string;
+};
 
 const itemsNameInHeader: itemsHeader[] = [
 	{name: 'Главная', link: '/'},
 	{name: 'Категории', link: '/categories'},
-	{name: 'Новости', link: routesMap.news},
-	{name: 'Акции', link: routesMap.discounts},
-	{name: 'Где купить', link: '/where-to-buy'},
-	{name: 'Доставка и оплата', link: routesMap.paymentAndDelivery},
-	{name: 'О нас', link: routesMap.aboutPage},
-	{name: 'Партнёрам', link: '/partners'}]
+];
 
 const MiniHeader: FC = () => {
 	return (
 		<div className={'miniHeader'}>
 			<div className="innerLinks">
-				{
-					itemsNameInHeader.map((item, index) => {
-						return (
-								<NavLink
-									key={index}
-									to={item.link}>
-									{item.name}
-								</NavLink>
-						)
-					})
-				}
+				{itemsNameInHeader.map((item, index) => {
+					return (
+						<NavLink key={index} to={item.link}>
+							{item.name}
+						</NavLink>
+					);
+				})}
 			</div>
 		</div>
-	)
-}
-export default MiniHeader
+	);
+};
+export default MiniHeader;
